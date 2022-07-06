4ad284a5f6f43e32440627e410d9661fb86d46fc
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2022-07-05 20:33:47 -0700

    Updates room page to add msg rooms

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'python is awesome!'},
+    {'id': 2, 'name': 'javascript is awesomeer!'},
+    {'id': 3, 'name': 'coding templates'},
+]
+
 # Create your views here.
 
 def home(req):
     return render(req, 'home.html')
 
 def room(req):
-    return render(req, 'room.html')
+    context = {'rooms': rooms}
+    return render(req, 'room.html', context)
